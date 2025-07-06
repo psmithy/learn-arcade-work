fcc634df40857a6687c114572b24483f4e2e04b7
Paul Smith <paulsmith1044@gmail.com>
2025-07-06 01:27:11 -0300

    Added Chapter 08 notes

---
+++ Testing/Chapter 08 - Functions/simple_function.py
@@ -0,0 +1,10 @@
+# Add two numbers and return the results
+def sum_two_numbers(a, b):
+    result = a + b
+    return result
+
+# Capture the result. If this isn't done, "result" only exists inside the function!
+my_result = sum_two_numbers(7, 11)
+
+# Print the captured result
+print(my_result)
